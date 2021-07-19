f37eaa373028274c60c82f476399d3be3ad73cb9
Borja Lopez <borja.lopez@aplazame.com>
2021-07-19 22:23:33 +0200

    Get prime numbers with set

--- numeros_primos/main.py
+++ numeros_primos/main.py
@@ -12,3 +12,19 @@ for num in range(4, 101):
     if primo(num):
         num_primos.append(num)
 print(num_primos)
+
+# With set()
+
+num_primos.clear()
+
+num_primos = {2, 3}
+
+for num in range(4, 101):
+    for div in range(2, num):
+        if num % div == 0:
+            if num in num_primos:
+                num_primos.remove(num)
+            break
+        num_primos.add(num)
+
+print(list(num_primos))
